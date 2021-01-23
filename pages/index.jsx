46c277cd3f832d0441e7686076ef9156de0b6f98
thaotram <sawa.kisei@gmail.com>
2021-01-23 16:02:49 +0700

    refactor(typescript): convert `_app` & `index` to tsx

--- pages/index.jsx
+++
@@ -1,3 +0,0 @@
-export default function Index() {
-    return <div className="flex justify-center p-8">index</div>;
-}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { DatePicker } from '../components/DatePicker';
+
+export default function Index() {
+    return (
+        <div className="flex justify-center p-8">
+            <DatePicker />
+        </div>
+    );
+}
